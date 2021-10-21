41a7bc96174090ccf15d63bb7e510cb8132a9f5f
Alex Silva <alexsilvademoura@gmail.com>
2021-10-21 19:55:00 -0300

    Adicionar arquivos de projeto.

---
+++ AppLanches/AppLanches/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppLanches.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppLanches.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppLanches/AppLanches/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppLanches.Models;
+using AppLanches.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLanches.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
